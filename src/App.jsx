98d8dfa0f47d8e64f25f5ad4eb76b24bef9b307a
Vadym <vadym_v@ukr.net>
2023-10-23 17:15:25 +0300

    init app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='app'>
+    <header className='app-header'>
+      <h1>Book library</h1>
+    </header>
+    </div>
+  )
+}
+
+export default App
